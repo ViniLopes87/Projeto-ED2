75c52ab78aa63d8da1a44f25f312f5b5cd4b34fb
Vinicius Lopes <viniciuslopes_2000@hotmail.com>
2021-03-15 12:29:06 -0300

    Add display method emOrdem, preOrdem and posOrdem

--- src/arvore/avl/TesteAVL.java
+++ src/arvore/avl/TesteAVL.java
@@ -3,14 +3,23 @@ package arvore.avl;
 public class TesteAVL {
     public static void main(String[] args) {
         ArvoreAVL a = new ArvoreAVL();
-        a.inserir(50);
-        a.inserir(60);
-        a.inserir(70);
-        a.inserir(80);
-        a.inserir(90);
-        a.inserir(100);
+        a.inserir(5);
+        a.inserir(2);
+        a.inserir(8);
+        a.inserir(3);
+        a.inserir(1);
+        a.inserir(7);
+        a.inserir(10);
+        a.inserir(9);
+        a.inserir(11);
+        a.deletar(7);
         
-        
+        a.posOrdem();
+        System.out.println();
+        a.preOrdem();
+        System.out.println();
+        a.emOrdem();
+        System.out.println();
         BinaryTreePrinter<No> p = new BinaryTreePrinter<No>(a);
         p.imprimir(System.out);
     }
